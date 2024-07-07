ca4efbdcfe399f85c46b97b75b5521ddb8e43fa7
Sohan Mekala <80227230+SohanMekala@users.noreply.github.com>
2024-07-07 18:57:12 -0500

    Update TemperatureSensor.ino

--- TemperatureSensor.ino
+++ TemperatureSensor.ino
@@ -1,1 +1,34 @@
+#include <Wire.h>
+#include <LCD_I2C.h>
+#include <DHT.h>
+
+LCD_I2C lcd(0x27,16,2);
+  
+#define DHTPIN 7  
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+int t;
+
+void setup()
+{
+    dht.begin();
+    lcd.begin();
+    lcd.backlight(); 
+}
+
+void loop()
+{
+
+    t = dht.readTemperature(); 
+
+    lcd.setCursor(0, 0);
+    lcd.println("Temperature     ");
+    
+    lcd.setCursor(0, 1);
+    lcd.print(t);
+    lcd.print("C");
+    
+  delay(1000);
+}
 
